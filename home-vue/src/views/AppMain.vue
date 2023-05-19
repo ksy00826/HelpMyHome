9f1c8be7fe0d9783553a545371155ba85ebdcd99
dlwns7267 <dlwns7267@naver.com>
2023-05-19 09:12:44 +0900

    날씨 import

--- home-vue/src/views/AppMain.vue
+++ home-vue/src/views/AppMain.vue
@@ -1,18 +1,20 @@
 <template>
   <b-container>
     <main-weather></main-weather>
-    <main-apt-graph></main-apt-graph>
     <main-news></main-news>
+    <main-apt-graph></main-apt-graph>
   </b-container>
 </template>
 
 <script>
-
-import MainAptGraph from '@/components/main/MainAptGraph.vue';
+import MainWeather from "@/components/main/MainWeather.vue";
+import MainAptGraph from "@/components/main/MainAptGraph.vue";
 import MainNews from "@/components/main/MainNews.vue";
+
 export default {
   name: "AppMain",
   components: {
+    MainWeather,
     MainAptGraph,
     MainNews,
   },
