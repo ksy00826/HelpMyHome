b18364952ca2a8ac99432c925ae64e817df99caf
dlwns7267 <dlwns7267@naver.com>
2023-05-22 11:32:47 +0900

    update: modal UI 개선

--- home-vue/src/components/user/LoginModal.vue
+++ home-vue/src/components/user/LoginModal.vue
@@ -8,91 +8,37 @@
     aria-labelledby="staticBackdropLabel"
     aria-hidden="true">
     <div class="modal-dialog modal-lg">
-      <div
-        class="modal-content"
-        style="
-          border-radius: 30px;
-          box-shadow: 0px 0px 20px 0px rgb(240, 240, 240);
-        ">
-        <!-- Modal Header -->
-        <!-- <div class="modal-header">
+      <div class="modal-content">
+        <div class="modal-header">
           <h4 class="modal-title">
             <i class="bi bi-chat-left-dots-fill text-info"> 로그인</i>
           </h4>
           <button
             type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"></button>
-        </div> -->
-
-        <!-- Modal body -->
-        <section>
-          <div class="">
-            <h2>로그인</h2>
-          </div>
-        </section>
-        <!-- ##### Breadcumb Area End ##### -->
-
-        <!-- ##### Login Area Start ##### -->
-        <!-- <section class="login-area section-padding-100">
-          <div class="container">
-            <div class="row justify-content-center">
-              <div class="col-12 col-lg-8">
-                <div class="login-content">
-                  <h3>Welcome Back</h3>
-                  
-                  <div class="login-form">
-                    <form action="_login.php" method="post">
-                      <div class="form-group">
-                        <label for="exampleInputEmail1"
-                          >이메일 주소를 입력해주세요.</label
-                        >
-                        <input
-                          name="id"
-                          type="email"
-                          class="form-control"
-                          id="exampleInputEmail1"
-                          aria-describedby="emailHelp"
-                          placeholder="Enter E-mail" />
-                        <small id="emailHelp" class="form-text text-muted"
-                          ><i class="fa fa-lock mr-2"></i>We'll never share your
-                          email with anyone else.</small
-                        >
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputPassword1"
-                          >비밀번호를 입력해주세요.</label
-                        >
-                        <input
-                          name="pw"
-                          type="password"
-                          class="form-control"
-                          id="exampleInputPassword1"
-                          placeholder="Password" />
-                      </div>
-                      <button type="submit" class="btn oneMusic-btn mt-30">
-                        Login
-                      </button>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section> -->
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
         <div class="modal-body">
           <form action="${root }/user" method="post">
-            <input type="hidden" name="cmd" value="login" />
+            <input
+              type="hidden"
+              name="cmd"
+              value="login"
+              style="margin: auto" />
             <div class="mb-3">
               <i
                 class="bi bi-patch-question"
                 style="color: rgb(121, 2, 119)"></i>
-              <label for="question" class="form-label" style=""
-                >아이디 * :</label
+              <label for="question" class="form-label font-italic"
+                >아이디를 입력해주세요.</label
               >
               <input
                 type="text"
                 class="form-control"
+                placeholder="ID"
                 id="loginId"
                 name="id"
                 v-model="loginId" />
@@ -101,7 +47,9 @@
               <i
                 class="bi bi-patch-question"
                 style="color: rgb(121, 2, 119)"></i>
-              <label for="question" class="form-label">비밀번호 * :</label>
+              <label for="question" class="form-label font-italic"
+                >비밀번호를 입력해주세요.</label
+              >
               <input
                 type="password"
                 class="form-control"
@@ -112,13 +60,13 @@
             </div>
 
             <div class="modal-footer">
-              <button
-                type="button"
+              <b-button
+                class="pb-2; lg=4"
+                variant="outline-dark"
                 id="btn-login"
-                class="btn btn-primary btn-sm"
                 @click="login()">
                 로그인
-              </button>
+              </b-button>
             </div>
           </form>
         </div>
@@ -136,6 +84,7 @@ export default {
     return {
       loginId: "",
       loginPw: "",
+      modalShow: false,
     };
   },
   created() {},
@@ -163,4 +112,21 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.form-control {
+  width: 450px;
+  margin: auto;
+}
+.form-label {
+  position: relative;
+  left: 60px;
+  text-align: left;
+  float: left;
+}
+.modal-content {
+  border-radius: 30px;
+  box-shadow: 0px 0px 20px 0px rgb(240, 240, 240);
+  width: 600px;
+  margin: auto;
+}
+</style>
